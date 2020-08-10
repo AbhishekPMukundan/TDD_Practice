317d037c89befdca6c35f14b14dc0c5d9136a8f5
Abhishek <mailabhi390@gmail.com>
2020-08-10 12:25:02 +0100

    SignupFormValidator

---
+++ TDD_Practice/Signup/SignupConstants.swift
@@ -0,0 +1,18 @@
+//
+//  SignupConstants.swift
+//  TDD_Practice
+//
+//  Created by Abhishek on 10/08/2020.
+//  Copyright © 2020 apm. All rights reserved.
+//
+
+import Foundation
+
+struct SignupConstants {
+    static let firstNameMinLength = 2
+    static let firstNameMaxLength = 10
+    static let lastNameMinLength = 2
+    static let lastNameMaxLength = 12
+    static let passwordMinLength = 8
+    static let passwordMaxLength = 16
+}
